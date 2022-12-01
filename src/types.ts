e6aa4e7881d462f8e6ecaffea6374b00e05aae1c
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2022-12-01 21:38:13 +0800

    chore: rm useless type define

--- src/types.ts
+++
@@ -1,8 +0,0 @@
-declare module 'abi-decoder' {
-  interface decodedABI {
-    name: string;
-    params: {name: string; value: any; type: string}[];
-  }
-  export function addABI(abi: any): void;
-  export function decodeMethod(calldata: string): decodedABI;
-}
