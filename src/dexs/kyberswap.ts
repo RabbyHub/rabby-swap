75436f64174ff008fd0372d22b7d5265b338cb3b
DMY <147dmy@gmail.com>
2025-03-03 11:16:47 +0800

    feat: openocean kyberswap add bera mnt

--- src/dexs/kyberswap.ts
+++ src/dexs/kyberswap.ts
@@ -24,6 +24,8 @@ export const SUPPORT_CHAINS = [
   // CHAINS_ENUM.BTT,
   CHAINS_ENUM.BASE,
   "SONIC" as CHAINS_ENUM,
+  CHAINS_ENUM.MANTLE,
+  "BERA" as CHAINS_ENUM,
   // No prev2 support
   // CHAINS_ENUM.ROSE,
 ];
